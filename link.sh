15586cc72ef52b82553ffa7b412e22467f895ad4
edluffy <edward@ncade.com>
2020-05-10 17:30:54 +0100

    Neovim + vim

--- link.sh
+++ link.sh
@@ -1,19 +1,22 @@
 sudo -v 
 
 # Delete old dotfiles
-sudo rm -rf ~/.config > /dev/null 2>&1
+sudo rm -rf ~/.config/kitty > /dev/null 2>&1
+sudo rm -rf ~/.config/nvim > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.vim > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 
 SYMLINKS=()
 
-# 'config' folder symlinks
 mkdir -p ~/.config
+mkdir -p ~/.config/nvim
+
+# 'config' folder symlinks
 sudo ln -sf ~/dotfiles/kitty ~/.config/kitty
 SYMLINKS+=('.config/kitty')
-sudo ln -sf ~/dotfiles/git ~/.config/git
-SYMLINKS+=('.config/git')
+sudo ln -sf ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim
+SYMLINKS+=('.config/nvim/init.vim')
 
 # 'home' folder symlinks
 sudo ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
