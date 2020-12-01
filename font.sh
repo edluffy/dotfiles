11ef7d64f43de94e5688bb0f07dc785b8cbddc2d
edluffy <edward@ncade.com>
2020-12-01 11:21:44 +0000

    Add emacs config + other small tweaks

--- font.sh
+++ font.sh
@@ -11,4 +11,5 @@ curl -fLo "Hack Italic Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd
 curl -fLo "Hack Bold Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf
 curl -fLo "Hack Bold Italic Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/BoldItalic/complete/Hack%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
 
-echo -e "$(tput bold)$(tput setaf 5)  Dotfiles: $(tput setaf 2)font.sh installed 'Hack'font\n"
+curl -fLo "Roboto Mono Light Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Light/complete/Roboto%20Mono%20Light%20Nerd%20Font%20Complete.ttf
+echo -e "$(tput bold)$(tput setaf 5)  Dotfiles: $(tput setaf 2)font.sh installed fonts\n"
