e432cc9a5782ef77d3693a68c3e12eefef8de633
edluffy <edward@ncade.com>
2021-07-23 14:40:37 +0100

    fix jetbrains font install

--- font.sh
+++ font.sh
@@ -6,7 +6,7 @@ else
 	exit 1
 fi
 
-curl -fLo "JetBrains Mono Regular Nerd Font Complete Mono.ttf"  https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 
 echo -e "$(tput bold)$(tput setaf 5)  Dotfiles: $(tput setaf 2)font.sh installed fonts\n"
 
